e297db37e648fa42e564de82a8a4570fd468e59f
kanaads <73217055+kanaads@users.noreply.github.com>
2023-04-16 20:48:26 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -2,23 +2,29 @@ from flask import Flask, render_template, request
 import requests
 import bs4
 import difflib
-import googlesearch
+import googlesearch as gs
 from bs4 import BeautifulSoup
 from googlesearch import search
 import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/run.py', methods=['POST'])
+
+@app.route('/search', methods=['POST'])
 def search():
     input_text = request.form['input_text']
-
     # Search for top 5 results for input text on Google
-    search_results = search(input_text, num_results=10)
+    search_results = gs.search(input_text, num_results=5)
+
+    # Create an empty list to store the similarity results
+    similarity_results = []
+    dict_res={}
+
 
     # Loop through each search result and extract content
     for result in search_results:
@@ -34,20 +40,19 @@ def search():
             similarity = difflib.SequenceMatcher(
                 None, input_text.lower(), content.lower()).ratio()
 
-            # Display the similarity and source of the content
-            outputFile = open("output.txt", 'a+')
-            outputFile.write((f"Similarity with {result}: {similarity}\n"))
-            outputFile.close()
+            # Add the similarity and source of the content to the list
+            similarity_results.append(f"Similarity with {result}: {similarity}\n")
+            dict_res[result]=similarity
         except Exception as e:
             # Handle any errors that occur during the search or extraction of content
             print(f"Error in processing {result}: {e}")
+        
 
-        # Perform Google search and similarity check as before
-
-        # Return a response with the similarity results
-        with open('output.txt', 'r') as f:
-            similarity_results = f.read()
-        return similarity_results
+    # Join the similarity results list into a single string with line breaks
+    output_text = '\n'.join(similarity_results)
+    
+    # Return a response with the similarity results
+    return dict_res
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
